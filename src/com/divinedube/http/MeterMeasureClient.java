867e5488322906703eabf8fd207aabd9de918300
Divine Dube <dubedivine@gmail.com>
2014-07-19 03:49:54 +0200

    tests my tests are working [hand tests] so move on to implementation of the tests

--- src/com/divinedube/http/MeterMeasureClient.java
+++ src/com/divinedube/http/MeterMeasureClient.java
@@ -10,18 +10,15 @@ import com.divinedube.metermeasure.BagOfValuesArray;
 import com.divinedube.metermeasure.MeterReadingsContract;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.savagelook.android.UrlJsonAsyncTask;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Divine Dube on 2014/07/15.
@@ -31,8 +28,10 @@ public class MeterMeasureClient extends IntentService{
     private static final String TAG="MeterMeasureClient";
 
     BagOfValuesArray bva = new BagOfValuesArray();
-    Gson json = new GsonBuilder().serializeNulls().create();
+    Gson gson = new GsonBuilder().serializeNulls().create();
     String jsonOfDb;
+    JsonElement jsonElement;
+
     public MeterMeasureClient() {
         super(TAG);
     }
@@ -46,7 +45,7 @@ public class MeterMeasureClient extends IntentService{
         int numRows = c.getCount();
         Log.d(TAG, "there are number rows in meter currently is: " + numRows);
 
-        for (int i = 0; i < numRows; i++){ //get the json representative of the meter db
+        for (int i = 0; i < numRows; i++){ //get the gson representative of the meter db
             c.moveToNext();
             int _id = c.getInt(0);
             String day = c.getString(1);
@@ -59,43 +58,47 @@ public class MeterMeasureClient extends IntentService{
             bva.putArr();
         }
 
-        jsonOfDb = json.toJson(bva);
+        jsonElement =  gson.toJsonTree(bva);
+        jsonOfDb = gson.toJson(bva);
         Log.d(TAG, jsonOfDb);
 
-
-
-        DefaultHttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost("http://192.168.56.1/meters.json");
         try {
-            Log.d(TAG, "Trying to push data to the server");
-            StringEntity stringEntity = new StringEntity(jsonOfDb);
-            Log.d(TAG, "NEEDED DATA*****  now here 1");
-            stringEntity.setContentType("application/json;charset=UTF-8");
-            Log.d(TAG, "NEEDED DATA*****  now here 2");
-            stringEntity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
-            Log.d(TAG, "NEEDED DATA*****  now here 3");
-            post.setHeader("Accept", "application/json");
-            Log.d(TAG, "NEEDED DATA*****  now here 4");
-            post.setEntity(stringEntity);
-            Log.d(TAG, "NEEDED DATA*****  now here 5");
-            HttpResponse response = client.execute(post);
-            Log.d(TAG, "NEEDED DATA***** " + response.toString());
-        }catch (UnsupportedEncodingException ese){
-            ese.printStackTrace();
-        }catch (IOException ioe){
-            ioe.printStackTrace();
+            JSONObject jsObj = new JSONObject(jsonOfDb);
+            int val = jsObj.length();
+           JSONArray js =  jsObj.getJSONArray("meter");
+            int leng = js.length();
+
+            Log.d(TAG, "the id of the object at index 1 is " + js.getJSONObject(0).getString("id") + "the length of the array is " + leng);
+
+            Log.d(TAG, "the number of object is " + val);
+        }catch (JSONException jse){
+            jse.printStackTrace();
         }
     }
 
     private class PullOrPushReadings extends UrlJsonAsyncTask {
 
+//
         public PullOrPushReadings(Context context) {
             super(context);
         }
-
-        @Override
-        protected JSONObject doInBackground(String... urls) {
-            return super.doInBackground(urls);
-        }
+//        @Override
+//        protected JSONObject doInBackground(String... urls) {
+//            DefaultHttpClient client = new DefaultHttpClient();
+//            HttpPost post = new HttpPost(urls[0]);
+//            JSONObject holder = new JSONObject();
+//            JSONObject meterObj = new JSONObject();
+//            String response = null;
+//            JSONObject json = new JSONObject();
+//
+////            try{
+////                try {
+////                    json.put("success", true);
+////                    json.put("info", "from android to server");
+////
+////
+////                }
+////            }
+//        }
     }
 }
